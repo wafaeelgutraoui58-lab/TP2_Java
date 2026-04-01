4a5244d434af63b85721870a65461f5624f3e75a
HP <HP@DESKTOP-30OMPIO>
2026-04-01 23:08:01 +0100

    Version finale du TP2

---
+++ TP2_Java/src/tp2/Banque.java
@@ -0,0 +1,25 @@
+package tp2;
+public class Banque {
+    private String nom;
+    private CompteBancaire[] comptes;
+    private int nbActuels;
+    public Banque(String nom, int capaciteMaximale) {
+        this.nom = nom;
+        this.comptes = new CompteBancaire[capaciteMaximale];
+        this.nbActuels = 0;
+    }
+    public void ajouterCompte(CompteBancaire c) {
+        if (nbActuels < comptes.length) {
+            comptes[nbActuels] = c;
+            nbActuels++;
+        } else {
+            System.out.println("Banque pleine, impossible d'ajouter un compte.");
+        }
+    }
+    public void afficherTous() {
+        System.out.println("--- Liste des comptes de la banque " + nom + " ---");
+        for (int i = 0; i < nbActuels; i++) {
+            comptes[i].afficher();
+        }
+    }
+}
